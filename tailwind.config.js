3c6925560029410e6c428bf3ff5d1214311a0a88
ibehzadi <bsd098@gmail.com>
2021-10-10 10:46:27 +0330

    initialize project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './src/**/*.{ts,vue}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
