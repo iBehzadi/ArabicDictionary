218523fe1c9f54339649dbaa023275db087ec28b
ibehzadi <bsd098@gmail.com>
2021-10-19 21:02:14 +0330

    add proxy to config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+/**
+ * @type {import('vite').UserConfig}
+ */
 // https://vitejs.dev/config/
 export default defineConfig({
+  server : {
+    proxy : {
+      '/api' : 'https://nebrasar.ir/'
+    }
+  },
   plugins: [vue()],
   resolve: {
     alias: {
@@ -10,3 +18,5 @@ export default defineConfig({
     },
   }
 })
+
+
