99941824ff4bfa5c8d8733bca3963c6a6565fa0d
Mohamad <m.sanjarmoosavi@gmail.com>
2021-10-20 17:39:04 +0330

    Normalize Alias '@'

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// @ts-ignore
 import path from 'path'
 /**
  * @type {import('vite').UserConfig}
  */
 // https://vitejs.dev/config/
 export default defineConfig({
-  server : {
-    proxy : {
-      '/api' : 'https://nebrasar.ir/'
+  server: {
+    proxy: {
+      '/api': 'https://nebrasar.ir/'
     }
   },
   plugins: [vue()],
   resolve: {
     alias: {
+      // @ts-ignore
       '@': path.resolve(__dirname, './src'),
     },
   }
