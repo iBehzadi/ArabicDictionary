74435306bed2a839c7e80dd346360863851fdf77
Mohamad <m.sanjarmoosavi@gmail.com>
2021-10-22 10:22:51 +0330

    The connections between the database and API are specified in this file(for Word)

---
+++ src/repo/Word.ts
@@ -0,0 +1,8 @@
+import { defineStore } from "pinia";
+import db from "@/database/WordDB";
+import { IWord } from "@/database/WordDB"
+export const useWordDB = defineStore('WordDB', {
+  actions: {
+ 
+  }
+})
