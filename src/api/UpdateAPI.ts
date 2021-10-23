8f8921e96bd208e5b5d0f3c5e8dfde0ceb9ac6ad
Mohamad <m.sanjarmoosavi@gmail.com>
2021-10-23 12:03:02 +0330

    Update API compeleted

--- src/api/UpdateAPI.ts
+++ src/api/UpdateAPI.ts
@@ -1,14 +1,16 @@
 import { httpGet } from ".";
 
-export function getUpdates(lastUpdate: string | null) {
+export function getUpdates_API(lastUpdate: string | null) {
     if (lastUpdate == null) lastUpdate = '-1'
-    type Output = {
-        lastUpdate: number
-        words: IWord[]
-        categories: ICategory[]
-    };
+
 
     return httpGet<Output>('/api/getUpdates.php?variant=normal&lastUpdate=' + lastUpdate)
 }
 
-
+declare global {
+    type Output = {
+        lastUpdate: number
+        words: IWord[]
+        categories: ICategory[]
+    }
+}
