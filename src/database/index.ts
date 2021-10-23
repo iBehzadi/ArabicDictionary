e943ce5744acfac717694147773b6996cef3d763
Mohamad <m.sanjarmoosavi@gmail.com>
2021-10-23 12:04:23 +0330

    Add LastUpdate Table to DataBase

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,24 +2,28 @@
 import Dexie from "dexie";
 
 class WordDatabase extends Dexie {
+    lastUpdate: Dexie.Table<ILastUpdate, number>
     word: Dexie.Table<IWord, number>; // number = type of the primkey
     category: Dexie.Table<ICategory, number>;
     constructor() {
         super("WordDatabase");
         this.version(1).stores({
+            lastUpdate: "LastUpdate",
             category: "CategoryID,Title,IsFree,LastUpdate,Status",
             word: "WordID,CategoryID,Fa,Ar,ReferTo,Dialect,LastUpdate"
         });
-        this.word = this.table("word");
+        this.lastUpdate = this.table("lastUpdate");
+            this.word = this.table("word");
         this.category = this.table("category");
     }
 }
 
 let db = new WordDatabase()
 
-export default db
+export default db;
 
 declare global {
+    interface ILastUpdate { LastUpdate: number }
     interface IWord {
         WordID: number,
         CategoryID: number,
@@ -35,7 +39,7 @@ declare global {
     }
     interface ICategory {
         CategoryID: number,
-        Title : string,
+        Title: string,
         Icon: string,
         CustomOrder: number,
         IsFree: number,
