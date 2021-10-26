a3ed7e5a0cd6933aeedb247537acbd070e5a7336
Mohamad <m.sanjarmoosavi@gmail.com>
2021-10-26 09:33:36 +0330

    change DB folders

--- src/database/CategoryDB.ts
+++ src/database/CategoryDB.ts
@@ -1,6 +1,9 @@
 import db from "."
 
-export async function CategoryDB_getAll(){
+export function CategoryDB_AddAll(res: ICategory[]) {
+   db.category.bulkPut(res)
+};
+export async function CategoryDB_GetAll(){
      return db.category.orderBy('CategoryID').toArray()
       
   }
--- src/database/WordDB.ts
+++ src/database/WordDB.ts
@@ -1,6 +1,5 @@
 import db from ".";
 
-async addAll(word: IWord[]) {
-    const count = await db.word.bulkPut(word);
-    console.log('inserted ' + count);
-  }
+export function WordDB_AddAll(res: IWord[]) {
+  db.word.bulkPut(res);
+};
