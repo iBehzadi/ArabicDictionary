f23de5dc9b3b4f5f72dd59ff8b44b18f1583850b
Mohamad <m.sanjarmoosavi@gmail.com>
2021-10-28 10:00:57 +0330

    Add searchDB_AddWordsInDB

--- src/repo/Update.ts
+++ src/repo/Update.ts
@@ -1,27 +1,26 @@
 import { getUpdates_API } from "@/api/UpdateAPI";
 import db from "@/database";
 import { CategoryDB_AddAll } from "@/database/CategoryDB";
-import { LastUpdate_Get} from "@/database/LastUpdateDB";
+import { LastUpdate_Get } from "@/database/LastUpdateDB";
 import { WordDB_AddAll } from "@/database/WordDB";
 import { defineStore } from "pinia";
-
-export const useUpdateRepo = defineStore('UpdateDB', {
+import { searchDB_AddWordsInDB } from "@/database/SearchDB";
+export const useUpdateRepo = defineStore("UpdateDB", {
   state: () => {
     return {
       lastCheck: Date,
       Data: {} as Output,
-    }
+    };
   },
   actions: {
     async LastUpdateCheked() {
-let lastUpdate = LastUpdate_Get()
-      this.Data = await getUpdates_API((await lastUpdate.toString()));
-      if (this.Data.lastUpdate > 0) {
+      let lastUpdate = LastUpdate_Get();
+      this.Data = await getUpdates_API(await lastUpdate.toString());
+      if (this.Data.lastUpdate != 0) {
         CategoryDB_AddAll(this.Data.categories);
         WordDB_AddAll(this.Data.words);
-      }
-    }
-  }
+        searchDB_AddWordsInDB(this.Data.words);
+      };
+    },
+  },
 });
-
-
