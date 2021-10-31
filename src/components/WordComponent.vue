58128428276ccc7d0b708cbe14ac1d882861f9ea
Mohamad <m.sanjarmoosavi@gmail.com>
2021-10-31 18:04:26 +0330

    Add checked Class for bookmark

--- src/components/WordComponent.vue
+++ src/components/WordComponent.vue
@@ -1,17 +1,31 @@
-<script setup lang='ts'>
-
-import { defineProps } from "vue";
+<script setup lang="ts">
+import { computed, defineProps } from "vue";
+import { useBookmarksRepo } from "@/repo/Bookmarks";
 const props = defineProps<{
-  words: IWord[]
+  words: IWord[];
 }>();
-
+// isBookmark: false;
+const bookmarksRepo = useBookmarksRepo();
+let bookmarks = computed(() => bookmarksRepo.$state.bookmarks);
+bookmarks.value.includes(w.WordId);
 </script>
 
 <template>
   <!-- کامپوننت لغات   -->
   <div
-    class="w-full h-auto shadow-sm border flex bg-gray-50 mb-1 rounded-xl odd:bg-gray-300 mt-1"
-    v-for="(item,i) in words"
+    class="
+      w-full
+      h-auto
+      shadow-sm
+      border
+      flex
+      bg-gray-50
+      mb-1
+      rounded-xl
+      odd:bg-gray-300
+      mt-1
+    "
+    v-for="(item, i) in words"
     :key="i"
   >
     <div class="flex-grow text-sm pt-1 pr-2">
@@ -20,19 +34,27 @@ const props = defineProps<{
       </div>
       <div class="font-black pb-1">{{ item.Ar }}</div>
       <div class="text-gray-600 pb-1">{{ item.Fa }}</div>
-      <div class="text-gray-400 pb-1" v-if="item.Example">مثال: {{ item.Example }}</div>
+      <div class="text-gray-400 pb-1" v-if="item.Example">
+        مثال: {{ item.Example }}
+      </div>
     </div>
 
     <div class="flex items-center">
       <button class="w-12 h-12 flex-center">
-        <font-awesome-icon :icon="['fas', 'bookmark']" class="text-sm text-gray-600" />
+        <font-awesome-icon
+          :icon="['fas', 'bookmark']"
+          class="text-sm text-gray-600"
+          :class="{ 'bg-black': bookmarks.includes(item.WordID) }"
+        />
       </button>
       <button class="w-12 h-12 flex-center">
-        <font-awesome-icon :icon="['fas', 'play']" class="text-sm text-gray-600" />
+        <font-awesome-icon
+          :icon="['fas', 'play']"
+          class="text-sm text-gray-600"
+        />
       </button>
     </div>
   </div>
 </template>
 
-<style>
-</style>
+<style></style>
