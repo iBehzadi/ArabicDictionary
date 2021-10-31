fcbd1db3ccb77ef320c7211d5f544b50fbc058dc
Mohamad <m.sanjarmoosavi@gmail.com>
2021-10-31 17:19:32 +0330

    hot fix

--- src/database/BookmarksDB.ts
+++ src/database/BookmarksDB.ts
@@ -1,15 +1,15 @@
 import db from ".";
 
 export async function BookmarksDB_ChangeStatusWord(Bookmark: IBookmark) {
-  let wordStatus = await db.bookmark.where("WordID").equals(Bookmark.WordID).toArray;
-  if (wordStatus != null) {
+  let wordStatus = await db.bookmark.where("WordID").equals(Bookmark.WordID).count();
+  if (wordStatus == 1) {
     //Deleted
-    db.bookmark.where("WordID").equals(Bookmark.WordID).delete;
-  } else {
+    db.bookmark.where("WordID").equals(Bookmark.WordID).delete();
+  } else { 
     //Add to DB
      db.bookmark.put(Bookmark)
   }
 }
 export async function BookmarksDB_GetAll() {
-  return await db.bookmark.toArray;
+  return await db.bookmark.toArray();
 }
